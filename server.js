b38b8adbce7b0389dddb34d23bb7b6150a6e0f9e
Nick Scholz <nickscholz1003@gmail.com>
2025-03-10 21:30:58 -0400

    add db connection

--- server.js
+++ server.js
@@ -1,12 +1,37 @@
+require('dotenv').config();
 const express = require("express");
 const cors = require("cors");
 const port = process.env.PORT || 3000;
+const { Pool } = require("pg");
+
 const app = express();
 app.use(express.json());
 app.use(cors());
 
+const pool = new Pool({
+    connectionString: process.env.DATABASE_URL,
+    ssl: {
+        rejectUnauthorized: false
+    }
+});
+
 app.get("/", (req, res) => {
     res.send("Hello World");
+});
+
+app.get("/testdb", async (req, res) => {
+    try {
+        const client = await pool.connect();
+        const result = await client.query("SELECT * from leaderboard;");
+        const results = result ? result.rows : null ;
+        response_string = `Hello World! Current time from DB: ${JSON.stringify(results)}`;
+        res.send(response_string);
+        
+        client.release();
+    } catch (err) {
+        console.error(err);
+        res.send("Error " + err);
+    }
 });
 
 
